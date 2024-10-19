6a2d8a9e9fbcb297674e59cae740e9ef036a53e3
Fernanda-Vicente <fernandavicenteg31@gmail.com>
2024-10-19 12:04:18 -0600

    Add files via upload

--- examen1.py
+++ examen1.py
@@ -1,7 +1,7 @@
 name=input("Ingrese su nombre: ")
 calificación1= int(input("Ingrese la primera calificación (de 0 a 100): "))
 calificación2= int(input("Ingrese la segunda calificación (de 0 a 100): "))
-calificación3= int(input("Ingrese la tercera calificaqcion (de 0 a 100): "))
+calificación3= int(input("Ingrese la tercera calificacion (de 0 a 100): "))
 promedio=(calificación1+calificación2+calificación3) /3
 
 print("{:<25}" .format("Nombre"))
