1ac55cb3461a8c177cba8fb9f1d5a2c80acd7e2e
Fernanda-Vicente <fernandavicenteg31@gmail.com>
2025-03-29 20:16:49 -0600

    Add files via upload

---
+++ actividad4tabla.py
@@ -0,0 +1,11 @@
+productos=["peluche", "muñeca", "pelota", "carrito", "zapatos"]
+precios= [40, 60, 25,30, 120]
+i=0                                #el listado empieza del 0
+while i<4:
+ maximum=max(precios)       
+ pos_max=precios.index(maximum)
+ name=productos[pos_max]
+ print(name, maximum)     #producto y precio
+ del productos[pos_max]            #borrar el producto ya seleccionado
+ del precios[pos_max]              #borrar el precio ya seleccionado 
+ i+=1   
